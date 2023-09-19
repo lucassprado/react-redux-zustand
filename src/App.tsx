adda2f2462f654c1a15816e1c47b5a681ec8fcd5
Lucas Prado <lucasntc2201@gmail.comq:qq>
2023-09-19 08:26:47 -0300

    chore: creating player store

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
-import { Player } from './pages/Player'
 import './styles/global.css'
+
+import { Provider as ReduxProvider } from 'react-redux'
+
+import { Player } from './pages/Player'
+import { store } from './store'
 
 export function App() {
   return (
-    <Player />
+    <ReduxProvider store={store}>
+      <Player />
+    </ReduxProvider>
   )
 }
